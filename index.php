65ceb9d92233268586da96db5ab402738be70476
Corey Stanford <corey.michael.stanford@gmail.com>
2015-02-21 16:45:29 -0500

    anonymous index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+		<?php 
+
+		include 'view/anonymous-header.php'; 
+		$url = "images/featured1.jpg";
+
+		?>
+`
+		<section role=main>
+			<div class="main-img">	
+				<div class="featured"></div>
+			</div>
+			<img class="off" id="invisible" src="<?php echo $url ?>" />;å
+		</section><!-- END main section -->
+		
+		<?php include 'view/anonymous-footer.php'; ?>
