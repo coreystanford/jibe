ae659c6ee7892d805f312cc766b1dbff03f604c7
Corey Stanford <corey.michael.stanford@gmail.com>
2015-03-26 00:13:25 -0400

    explore.php

---
+++ explore/explore.php
@@ -0,0 +1,19 @@
+<?php include '../view/header.php'; ?>
+
+	<section role=main>
+		
+		<div class="feed">
+
+		</div>
+
+		<div class="load-more clearfix">
+
+			<a href="#" id="load-more"><span>Load More <i class="fa fa-chevron-down fa-lg"></i></span></a>
+
+		</div>
+
+	</section><!-- END main section -->
+	
+<?php include '../view/footer.php'; ?>
+
+<div id="modal"></div>
