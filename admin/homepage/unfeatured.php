1095caf1777bb19a17b84f3acd6113279e702925
Corey Stanford <corey.michael.stanford@gmail.com>
2015-03-27 14:02:38 -0400

    unfeatured.php

---
+++ admin/homepage/unfeatured.php
@@ -0,0 +1,30 @@
+	<?php include '../view/header.php'; ?>
+
+	<section role=main>
+
+		<div class="main-admin">
+			
+			<h1>Homepage</h1>
+
+			<div class="featured-proj">
+			
+				<a href="../homepage" class="edit"><i class="fa fa-arrow-left fa-lg"></i> Return </a>
+
+				<h2>Add Featured Projects: </h2>
+
+				<div class="feed">
+
+				</div>
+				
+				<div class="load-more clearfix">
+
+					<a href="#" id="load-more"><span>Load More <i class="fa fa-chevron-down fa-lg"></i></span></a>
+
+				</div>
+
+			</div>
+		</div>
+
+	</section><!-- END main section -->
+	
+	<?php include '../view/footer.php'; ?>
