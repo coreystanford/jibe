b76081730cec04627261a4a0c8a71d626cb2ef93
Corey Stanford <corey.michael.stanford@gmail.com>
2015-03-28 02:27:42 -0400

    resize_crop_image() - thumbnails from centre

--- model/fileupload.php
+++ model/fileupload.php
@@ -122,86 +122,63 @@ class FileUpload {
         $image_path_pro = $dir . "profiles/" . $image_name . $ext;
         $image_path_feed = $dir . "thumbs/" . $image_name . $ext;
 
-        self::resize_image($image_path, $image_path_pro, 225, 169);
-        self::resize_image($image_path, $image_path_feed, 106, 80);
+        self::resize_crop_image(225, 169, $image_path, $image_path_pro);
+        self::resize_crop_image(106, 80, $image_path, $image_path_feed);
     }
 
-    private function resize_image($old_image_path, $new_image_path,
-            $max_width, $max_height) {
+    // http://polyetilen.lt/en/resize-and-crop-image-from-center-with-php
 
-        // Get image type
-        $image_info = getimagesize($old_image_path);
-        $image_type = $image_info[2];
-
-        // Set up the function names
-        switch($image_type) {
-            case IMAGETYPE_JPEG:
-                $image_from_file = 'imagecreatefromjpeg';
-                $image_to_file = 'imagejpeg';
+    //resize and crop image by center
+    public function resize_crop_image($max_width, $max_height, $source_file, $dst_dir, $quality = 80){
+        $imgsize = getimagesize($source_file);
+        $width = $imgsize[0];
+        $height = $imgsize[1];
+        $mime = $imgsize['mime'];
+     
+        switch($mime){
+            case 'image/gif':
+                $image_create = "imagecreatefromgif";
+                $image = "imagegif";
                 break;
-            case IMAGETYPE_GIF:
-                $image_from_file = 'imagecreatefromgif';
-                $image_to_file = 'imagegif';
+     
+            case 'image/png':
+                $image_create = "imagecreatefrompng";
+                $image = "imagepng";
+                $quality = 7;
                 break;
-            case IMAGETYPE_PNG:
-                $image_from_file = 'imagecreatefrompng';
-                $image_to_file = 'imagepng';
+     
+            case 'image/jpeg':
+                $image_create = "imagecreatefromjpeg";
+                $image = "imagejpeg";
+                $quality = 80;
                 break;
+     
             default:
-                echo 'File must be a JPEG, GIF, or PNG image.';
-                exit;
+                return false;
+                break;
         }
-
-        // Get the old image and its height and width
-        $old_image = $image_from_file($old_image_path);
-        $old_width = imagesx($old_image);
-        $old_height = imagesy($old_image);
-
-        // Calculate height and width ratios
-        $width_ratio = $old_width / $max_width;
-        $height_ratio = $old_height / $max_height;
-
-        // If image is larger than specified ratio, create the new image
-        if ($width_ratio > 1 || $height_ratio > 1) {
-
-            // Calculate height and width for the new image
-            $ratio = max($width_ratio, $height_ratio);
-            $new_height = round($old_height / $ratio);
-            $new_width = round($old_width / $ratio);
-
-            // Create the new image
-            $new_image = imagecreatetruecolor($new_width, $new_height);
-
-            // Set transparency according to image type
-            if ($image_type == IMAGETYPE_GIF) {
-                $alpha = imagecolorallocatealpha($new_image, 0, 0, 0, 127);
-                imagecolortransparent($new_image, $alpha);
-            }
-            if ($image_type == IMAGETYPE_PNG || $image_type == IMAGETYPE_GIF) {
-                imagealphablending($new_image, false);
-                imagesavealpha($new_image, true);
-            }
-
-            // Copy old image to new image - this resizes the image
-            $new_x = 0;
-            $new_y = 0;
-            $old_x = 0;
-            $old_y = 0;
-            imagecopyresampled($new_image, $old_image,
-                               $new_x, $new_y, $old_x, $old_y,
-                               $new_width, $new_height, $old_width, $old_height);
-
-            // Write the new image to a new file
-            $image_to_file($new_image, $new_image_path);
-
-            // Free any memory associated with the new image
-            imagedestroy($new_image);
-        } else {
-            // Write the old image to a new file
-            $image_to_file($old_image, $new_image_path);
+         
+        $dst_img = imagecreatetruecolor($max_width, $max_height);
+        $src_img = $image_create($source_file);
+         
+        $width_new = $height * $max_width / $max_height;
+        $height_new = $width * $max_height / $max_width;
+        //if the new width is greater than the actual width of the image, then the height is too large and the rest cut off, or vice versa
+        if($width_new > $width){
+            //cut point by height
+            $h_point = (($height - $height_new) / 2);
+            //copy image
+            imagecopyresampled($dst_img, $src_img, 0, 0, 0, $h_point, $max_width, $max_height, $width, $height_new);
+        }else{
+            //cut point by width
+            $w_point = (($width - $width_new) / 2);
+            imagecopyresampled($dst_img, $src_img, 0, 0, $w_point, 0, $max_width, $max_height, $width_new, $height);
         }
-        // Free any memory associated with the old image
-        imagedestroy($old_image);
+         
+        $image($dst_img, $dst_dir, $quality);
+     
+        if($dst_img)imagedestroy($dst_img);
+        if($src_img)imagedestroy($src_img);
     }
     
 }
