c9f3bc523fc08ad7f70ff758403d7876c05505dc
coreystanford <corey.michael.stanford@gmail.com>
2015-04-22 10:59:03 -0400

    profile setup

--- profile/setup.php
+++ profile/setup.php
@@ -1,30 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="utf-8">
-  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-
-  <!-- Info -->
-  <title>PHP Project</title>
-  <meta name="description" content="">
-  <meta name="author" content="">
-  <!-- Viewport -->
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <!-- Fonts -->
-  <link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
-  <link href='http://fonts.googleapis.com/css?family=Arvo:400,700' rel='stylesheet' type='text/css'>
-  <!-- Icons -->
-  <link rel="stylesheet" href="../css/font-awesome/css/font-awesome.min.css">
-  <!-- Style -->
-  <link rel="stylesheet" href="../css/style.css">
-
-  <!-- Favicon -->
-  <link rel="shortcut icon" type="image/png" href="../images/favicon.png">
-</head>
-
-	<body>
-
-		<div id="wrapper">
+<?php include '../view/header.php'; ?>
 
       <div class="photo" >
 
@@ -81,34 +55,4 @@
       </form><!-- end form -->
 
 
-    </div><!-- end wrapper -->
-
-    <footer class="clearfix">
-
-      <nav>
-
-        <ul>
-          <li><a href="#">Site Map</a></li>
-          <li><a href="#">Terms of Use</a></li>
-          <li><a href="#">Privacy Policy</a></li>
-        </ul>
-
-      </nav><!-- end nav -->
-
-      <p>&copy;<?php echo date('Y'); ?> JIBE | All Rights Reserved</p>
-        
-    </footer><!-- end footer -->
-
-    <script src="http://code.jquery.com/jquery-1.10.2.js"></script>
-    <script type="text/javascript" src="../js/logo.js"></script>
-    <script type="text/javascript" src="../js/slider.js"></script>
-    <script type="text/javascript" src="../js/tab.js"></script>
-    <script type="text/javascript" src="../js/feed-modal.js"></script>
-    <script type="text/javascript" src="../js/feed-modal-init.js"></script>
-    <script type="text/javascript" src="../js/profile-follow.js"></script>
-    <script type="text/javascript" src="../js/img-modal.js"></script>
-    <script type="text/javascript" src="../js/job-board.js"></script>
-
-  </body><!-- end body -->
-  
-</html><!-- end html -->
+<?php include '../view/footer.php'; ?>
